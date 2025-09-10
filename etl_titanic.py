9e72c8dd7115e83e6cd8f3713d0d07c3b8935134
Anton Kurashkevich <mr.kur.anton@gmail.com>
2025-09-10 14:15:09 +0300

    feat: rename columns

--- etl_titanic.py
+++ etl_titanic.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # --- EXTRACT ---
 print("Starting Extract step...")
@@ -20,3 +21,22 @@ print(df.head())
 
 print("\nDataFrame Info:")
 df.info()
+
+def camel_to_snake(name):
+    """
+    Конвертирует строку из CamelCase в snake_case.
+    """
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+# --- TRANSFORM ---
+print("\nStarting Transform step...")
+
+# Шаг 1: Очистка названий колонок
+# Создаем копию DataFrame, чтобы не изменять оригинал напрямую.
+df_transformed = df.copy()
+
+df_transformed.columns = [camel_to_snake(col) for col in df_transformed.columns]
+
+print("Cleaned column names:")
+print(df_transformed.columns)
